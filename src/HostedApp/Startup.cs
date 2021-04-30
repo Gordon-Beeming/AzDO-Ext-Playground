a725722bcdf8ef52283aa1e82ac85d19eb993436
CI System <gordonbeeming@outlook.com>
2021-05-01 00:34:34 +0200

    remove x-frame-options from code as it's being managed in the worker in CF

--- src/HostedApp/Startup.cs
+++ src/HostedApp/Startup.cs
@@ -39,11 +39,6 @@ namespace HostedApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.Use(async (context, next) =>
-            {
-              context.Response.Headers.Add("X-Frame-Options", "ALLOW-FROM https://dev.azure.com");
-              await next();
-            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
